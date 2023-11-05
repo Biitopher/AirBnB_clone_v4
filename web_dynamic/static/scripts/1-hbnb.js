d7a748920adee480070e8234d232115121a2d78e
Evans Sese <sese_evans@yahoo.com>
2023-11-05 19:41:28 +0300

    [Feature] Task 2 complete

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  const checkedAmenities = {};
+
+  // Listen for changes on each input checkbox tag
+  $('input[type="checkbox"]').change(function () {
+    const checkbox = $(this);
+    const amenityID = checkbox.data('id');
+    const amenityName = checkbox.data('name');
+
+    if (checkbox.is(':checked')) {
+      // If the checkbox is checked, store the Amenity ID in the variable
+      checkedAmenities[amenityID] = amenityName;
+    } else {
+      // If the checkbox is unchecked, remove the Amenity ID from the variable
+      delete checkedAmenities[amenityID];
+    }
+
+    // Update the h4 tag inside the div Amenities with the list of Amenities checked
+    const amenitiesList = Object.values(checkedAmenities).join(', ');
+    $('#checked_amenities').text(amenitiesList);
+  });
+});
